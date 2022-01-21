d66245a55b799af7aa7798c597b0dfb7fce17ef5
Jakob Klausbauer <jakob.klausbauer@bulme.at>
2022-01-21 17:18:51 +0100

    Added Input/Output methods

--- webclient.cpp
+++ webclient.cpp
@@ -4,4 +4,24 @@ Webclient::Webclient(QWidget *parent) :
     QWidget(parent)
 {
     setupUi(this);
+
+    connect(u_address, &QLineEdit::returnPressed, this, &Webclient::on_u_goButton_clicked); //Pressing enter when entering URL starts the process as well
+}
+
+void Webclient::printMessage(QString message, QColor color)
+{
+    u_textOutp->clear();
+    u_textOutp->setTextColor(color);
+    u_textOutp->setText(message);
+}
+
+void Webclient::on_u_goButton_clicked()
+{
+    if(!u_address->text().isEmpty())
+    {
+        m_address = u_address->text();
+        printMessage("Input: " + m_address, QColor::fromRgb(0, 255, 0));
+    }
+    else
+        printMessage("Please enter an address!", QColor::fromRgb(255, 0, 0));
 }
--- webclient.h
+++ webclient.h
@@ -2,6 +2,9 @@
 #define WEBCLIENT_H
 
 #include "ui_webclient.h"
+#include <QDebug>
+#include <QAbstractSocket>
+#include <QTcpSocket>
 
 class Webclient : public QWidget, private Ui::Webclient
 {
@@ -9,6 +12,17 @@ class Webclient : public QWidget, private Ui::Webclient
 
 public:
     explicit Webclient(QWidget *parent = nullptr);
+
+private slots:
+    void on_u_goButton_clicked();
+
+private:
+    void printMessage(QString message, QColor color = QColor::fromRgb(0, 0, 0));
+
+private:
+    QTcpSocket* m_socket = nullptr;
+    QString m_address;
+    const unsigned char m_port = 80;
 };
 
 #endif // WEBCLIENT_H
